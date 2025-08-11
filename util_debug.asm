d748a5e4679ae5b3d193d60f99bfb5ffa910534e
zachmahan <zach.mahan.zm@gmail.com>
2025-08-11 19:40:57 -0400

    working on util_disk_io.asm, added a util_debug.asm

---
+++ util_debug.asm
@@ -0,0 +1,20 @@
+@include "text_processing.asm"
+.data
+    util_debug_true_str:
+        .string "TRUE"
+    util_debug_false_str:
+        .string "FALSE"
+.text
+util_debug_print_tf:
+    # a0 = TRUE/FALSE (1/0)
+    mov a1, 0
+    mov a2, 0
+    # ^ set up pos args for blit_strl_rom
+    ge util_debug_print_tf_true, a0, 0
+    mov a0, util_debug_false_str
+    call blit_strl_rom
+    ret
+    util_debug_print_tf_true:
+    mov a0, util_debug_true_str
+    call blit_strl_rom
+    ret
--- util_disk_io.asm
+++ util_disk_io.asm
@@ -1,5 +1,9 @@
 # UTIL_DISK_IO.ASM
 
+@include "util_debug.asm"
+
+.text
+
 # DISK OPERATIONS
 .const NO_OP = 0x00
 .const READ_BYTE_OP = 0x01
@@ -21,4 +25,29 @@
 .const DISK_STATUS = 6564
 
 util_busy_disk_write:
-    # a0 =
+    # a0 = ptr_to_data, a1 = length, a2 = dest_in_disk (offset), s10 = dest_in_disk (page)
+    # LOCALS SET UP ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+    .const UTIL_BUSY_DISK_WRITE_DATA_PTR_OFFS = -2
+    .const UTIL_BUSY_DISK_WRITE_LEN_OFFS = -4
+    .const UTIL_BUSY_DISK_WRITE_DEST_OFFSET_OFFS = -6
+    .const UTIL_BUSY_DISK_WRITE_PAGE_OFFS = -8
+    .const UTIL_BUSY_DISK_WRITE_LOCAL_VAR_SIZE = 8
+
+    sub sp, sp, UTIL_BUSY_DISK_WRITE_LOCAL_VAR_SIZE # set up frame for locals
+    sw fp, UTIL_BUSY_DISK_WRITE_DATA_PTR_OFFS, a0
+    sw fp, UTIL_BUSY_DISK_WRITE_LEN_OFFS, a1
+    sw fp, UTIL_BUSY_DISK_WRITE_DEST_OFFSET_OFFS, a2
+    sw fp, UTIL_BUSY_DISK_WRITE_PAGE_OFFS, s10
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
+
+    # RESTORE ARGS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+    lw a0, fp, UTIL_BUSY_DISK_WRITE_DATA_PTR_OFFS
+    lw a1, fp, UTIL_BUSY_DISK_WRITE_LEN_OFFS
+    lw a2, fp, UTIL_BUSY_DISK_WRITE_DEST_OFFSET_OFFS
+    lw s10, fp, UTIL_BUSY_DISK_WRITE_PAGE_OFFS
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+    ret
+
